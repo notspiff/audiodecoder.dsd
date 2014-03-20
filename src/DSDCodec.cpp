ae34079dd49a7d4132a6d7efa64105263aadef54
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-03-20 19:14:34 +0100

    add bitstream parsing for dff files (snatched from mpd project)

--- src/DSDCodec.cpp
+++ src/DSDCodec.cpp
@@ -20,7 +20,37 @@ The views and conclusions contained in the software and documentation are those 
 authors and should not be interpreted as representing official policies, either expressed
 or implied, of Sebastian Gesemann.
 
-XBMC interface Copyright 2014 Arne Morten Kvarving
+ * Copyright (C) 2003-2014 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ XBMC interface Copyright 2014 Arne Morten Kvarving
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 #include "xbmc/libXBMC_addon.h"
@@ -29,6 +59,7 @@ XBMC interface Copyright 2014 Arne Morten Kvarving
 #include "xbmc/AEChannelData.h"
 #include "dsd2pcm.hpp"
 #include <iostream>
+#include "ByteOrder.hxx"
 
 inline long myround(float x)
 {
@@ -99,7 +130,7 @@ void ADDON_Stop()
 //-----------------------------------------------------------------------------
 void ADDON_Destroy()
 {
-  XBMC=NULL;
+//  XBMC=NULL;
 }
 
 //-- HasSettings --------------------------------------------------------------
@@ -167,6 +198,265 @@ struct DSDContext
   int bytespersample;
 };
 
+struct DsdId {
+  char value[4];
+
+  bool Equals(const char *s) const
+  {
+    return memcmp(value, s, sizeof(value)) == 0;
+  }
+};
+
+class DffDsdUint64
+{
+  uint32_t hi;
+  uint32_t lo;
+public:
+  uint64_t Read() const
+  {
+    return (uint64_t(FromBE32(hi)) << 32) |
+            uint64_t(FromBE32(lo));
+  }
+};
+
+struct DsdiffHeader {
+  DsdId id;
+  DffDsdUint64 size;
+  DsdId format;
+};
+
+struct DsdiffChunkHeader {
+  DsdId id;
+  DffDsdUint64 size;
+
+  /**
+   * Read the "size" attribute from the specified header, converting it
+   * to the host byte order if needed.
+   */
+    uint64_t GetSize() const {
+      return size.Read();
+    }
+};
+
+/** struct for DSDIFF native Artist and Title tags */
+struct dsdiff_native_tag {
+  uint32_t size;
+};
+
+struct DsdiffMetaData {
+  unsigned sample_rate, channels;
+  bool bitreverse;
+  uint64_t chunk_size;
+  /** offset for artist tag */
+  int64_t diar_offset;
+  /** offset for title tag */
+  int64_t diti_offset;
+  char diar[61];
+  char diti[61];
+
+  DsdiffMetaData()
+  {
+    diar[0] = diti[0] = '\0';
+  }
+};
+
+/**
+ * Read and parse a "SND" chunk inside "PROP".
+ */
+static bool
+dsdiff_read_prop_snd(void* file, DsdiffMetaData *metadata, int64_t end_offset)
+{
+  DsdiffChunkHeader header;
+  while ((XBMC->GetFilePosition(file) + sizeof(header)) <= end_offset)
+  {
+    if (!XBMC->ReadFile(file, &header, sizeof(header)))
+      return false;
+
+    int64_t chunk_end_offset = XBMC->GetFilePosition(file) + header.GetSize();
+    if (chunk_end_offset > end_offset)
+      return false;
+
+    if (header.id.Equals("FS  "))
+    {
+      uint32_t sample_rate;
+      if (!XBMC->ReadFile(file, &sample_rate, sizeof(sample_rate)))
+        return false;
+
+      metadata->sample_rate = FromBE32(sample_rate);
+    } 
+    else if (header.id.Equals("CHNL"))
+    {
+      uint16_t channels;
+      if (header.GetSize() < sizeof(channels) ||
+          !XBMC->ReadFile(file, &channels, sizeof(channels)) ||
+          XBMC->SeekFile(file, chunk_end_offset, SEEK_SET) != chunk_end_offset)
+        return false;
+
+      metadata->channels = FromBE16(channels);
+    } 
+    else if (header.id.Equals("CMPR"))
+    {
+      DsdId type;
+      if (header.GetSize() < sizeof(type) ||
+          !XBMC->ReadFile(file, &type, sizeof(type)) ||
+          XBMC->SeekFile(file, chunk_end_offset, SEEK_SET) != chunk_end_offset)
+        return false;
+
+      if (!type.Equals("DSD "))
+        /* only uncompressed DSD audio data
+           is implemented */
+        return false;
+    } 
+    else
+    {
+      /* ignore unknown chunk */
+
+      if (XBMC->SeekFile(file, chunk_end_offset, SEEK_SET) != chunk_end_offset)
+        return false;
+    }
+  }
+
+  return XBMC->GetFilePosition(file) == end_offset;
+}
+
+/**
+ * Read and parse a "PROP" chunk.
+ */
+static bool dsdiff_read_prop(void* file, DsdiffMetaData *metadata,
+                             const DsdiffChunkHeader *prop_header)
+{
+  uint64_t prop_size = prop_header->GetSize();
+  int64_t end_offset = XBMC->GetFilePosition(file) + prop_size;
+
+  DsdId prop_id;
+  if (prop_size < sizeof(prop_id) ||
+      !XBMC->ReadFile(file, &prop_id, sizeof(prop_id)))
+    return false;
+
+  if (prop_id.Equals("SND "))
+    return dsdiff_read_prop_snd(file, metadata, end_offset);
+  else
+    /* ignore unknown PROP chunk */
+    return XBMC->SeekFile(file, end_offset, SEEK_SET) == end_offset;
+}
+
+static bool dsdiff_read_metadata(void* file, DsdiffMetaData *metadata,
+                                 DsdiffChunkHeader *chunk_header)
+{
+  DsdiffHeader header;
+  if (!XBMC->ReadFile(file, &header, sizeof(header)) ||
+      !header.id.Equals("FRM8") ||
+      !header.format.Equals("DSD "))
+    return false;
+
+  while (true)
+  {
+    if (!XBMC->ReadFile(file, chunk_header, sizeof(*chunk_header)))
+      return false;
+
+    if (chunk_header->id.Equals("PROP"))
+    {
+      if (!dsdiff_read_prop(file, metadata, chunk_header))
+        return false;
+    } 
+    else if (chunk_header->id.Equals("DSD "))
+    {
+      const uint64_t chunk_size = chunk_header->GetSize();
+      metadata->chunk_size = chunk_size;
+      return true;
+    } 
+    else
+    {
+      /* ignore unknown chunk */
+      const uint64_t chunk_size = chunk_header->GetSize();
+      int64_t chunk_end_offset = XBMC->GetFilePosition(file) + chunk_size;
+
+      if (XBMC->SeekFile(file, chunk_end_offset, SEEK_SET) != chunk_end_offset)
+        return false;
+    }
+  }
+}
+
+static void dsdiff_handle_native_tag(void* file, int64_t tagoffset, char* tag)
+{
+  if (XBMC->SeekFile(file,tagoffset,SEEK_SET) != tagoffset)
+    return;
+
+  struct dsdiff_native_tag metatag;
+
+  if (!XBMC->ReadFile(file, &metatag, sizeof(metatag)))
+    return;
+
+  uint32_t length = FromBE32(metatag.size);
+
+  /* Check and limit size of the tag to prevent a stack overflow */
+  if (length == 0 || length > 60)
+    return;
+
+  if (!XBMC->ReadFile(file, tag, length))
+    return;
+
+  tag[length] = '\0';
+  return;
+}
+
+bool dsdiff_read_metadata_extra(void* file, DsdiffMetaData *metadata,
+                                DsdiffChunkHeader *chunk_header)
+{
+  /* skip from DSD data to next chunk header */
+  if (XBMC->SeekFile(file, metadata->chunk_size, SEEK_CUR) <= 0)
+    return false;
+  if (!XBMC->ReadFile(file, chunk_header, sizeof(*chunk_header)))
+    return false;
+
+  /* Now process all the remaining chunk headers in the stream
+     and record their position and size */
+
+  int64_t size = XBMC->GetFileLength(file);
+  while (XBMC->GetFilePosition(file) < size)
+  {
+    uint64_t chunk_size = chunk_header->GetSize();
+
+    /* DIIN chunk, is directly followed by other chunks  */
+    if (chunk_header->id.Equals("DIIN"))
+      chunk_size = 0;
+
+    /* DIAR chunk - DSDIFF native tag for Artist */
+    if (chunk_header->id.Equals("DIAR"))
+    {
+      chunk_size = chunk_header->GetSize();
+      metadata->diar_offset = XBMC->GetFilePosition(file);
+    }
+
+    /* DITI chunk - DSDIFF native tag for Title */
+    if (chunk_header->id.Equals("DITI"))
+    {
+      chunk_size = chunk_header->GetSize();
+      metadata->diti_offset = XBMC->GetFilePosition(file);
+    }
+    if (chunk_size != 0)
+    {
+      if (XBMC->SeekFile(file, chunk_size, SEEK_CUR) <= 0)
+        break;
+    }
+
+    if (XBMC->GetFilePosition(file) < size)
+    {
+      if (!XBMC->ReadFile(file, chunk_header, sizeof(*chunk_header)))
+        return false;
+    }
+  }
+  /* done processing chunk headers, process tags if any */
+
+  if (metadata->diar_offset != 0)
+    dsdiff_handle_native_tag(file, metadata->diar_offset, metadata->diar);
+
+  if (metadata->diti_offset != 0)
+    dsdiff_handle_native_tag(file, metadata->diti_offset, metadata->diti);
+
+  return true;
+}
+
 void* Init(const char* strFile, unsigned int filecache, int* channels,
            int* samplerate, int* bitspersample, int64_t* totaltime,
            int* bitrate, AEDataFormat* format, const AEChannel** channelinfo)
@@ -183,22 +473,37 @@ void* Init(const char* strFile, unsigned int filecache, int* channels,
     return NULL;
   }
 
-  result->channels = 2; // TODO!
+  DsdiffMetaData metadata;
+  DsdiffChunkHeader chunk_header;
+  if (!dsdiff_read_metadata(result->file, &metadata, &chunk_header))
+  {
+    XBMC->CloseFile(result->file);
+    delete result;
+    return NULL;
+  }
+  dsdiff_read_metadata_extra(result->file, &metadata, &chunk_header);
+
+  result->channels = metadata.channels;
   result->lsbitfirst = 0;
   result->block = 16384;
   result->bytespersample=3;
   *channels = result->channels;
-  *samplerate = 352000;
+  *samplerate = 352000; // decoder currently always decimates to 352000
   *bitspersample = result->bytespersample*8;
-  *totaltime = XBMC->GetFileLength(result->file)/(352*result->channels);
+  *totaltime = metadata.chunk_size/metadata.channels*8/metadata.sample_rate*1000;
 
   result->dxds.resize(result->channels);
   result->buffer = new uint8_t[result->block*result->channels*result->bytespersample];
   result->buffer_size = 0;
 
   *format = AE_FMT_S24LE3;
-  static enum AEChannel map[3] = {AE_CH_FL, AE_CH_FR, AE_CH_NULL};
-  *channelinfo = map;
+  static enum AEChannel map[3][9] = {
+    {AE_CH_FL, AE_CH_FR, AE_CH_NULL},
+    {AE_CH_FL, AE_CH_FR, AE_CH_FC, AE_CH_LFE, AE_CH_BL, AE_CH_BR, AE_CH_NULL}, // 6 channels
+    {AE_CH_FL, AE_CH_FR, AE_CH_FC, AE_CH_LFE, AE_CH_BL, AE_CH_BR, AE_CH_SL, AE_CH_SR, AE_CH_NULL} // 8 channels
+  };
+                        
+  *channelinfo = map[(metadata.channels==2?0:(metadata.channels==6?1:2))];
 
   return result;
 }
@@ -253,6 +558,7 @@ int64_t Seek(void* context, int64_t time)
 bool DeInit(void* context)
 {
   DSDContext* ctx = (DSDContext*)context;
+  XBMC->CloseFile(ctx->file);
   delete[] ctx->buffer;
   delete ctx;
   return true;
@@ -261,7 +567,23 @@ bool DeInit(void* context)
 bool ReadTag(const char* strFile, char* title, char* artist,
              int* length)
 {
-  return false;
+  void* file = XBMC->OpenFile(strFile, 0);
+  if (!file)
+    return false;
+
+  DsdiffMetaData metadata;
+  DsdiffChunkHeader chunk_header;
+  if (!dsdiff_read_metadata(file, &metadata, &chunk_header))
+  {
+    XBMC->CloseFile(file);
+    return false;
+  }
+  dsdiff_read_metadata_extra(file, &metadata, &chunk_header);
+  strcpy(title,metadata.diar);
+  strcpy(artist,metadata.diti);
+  *length = metadata.chunk_size/metadata.channels*8/metadata.sample_rate;
+  XBMC->CloseFile(file);
+  return true;
 }
 
 int TrackCount(const char* strFile)
